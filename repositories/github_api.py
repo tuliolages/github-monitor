58f0fe0bee261cbb7c0dcaff729493252ed62a1e
Tulio Lages <tuliolages@joystreet.com.br>
2021-02-04 23:50:35 -0300

    update commit retireval procedure to get only commits from the last 30 days

--- repositories/github_api.py
+++ repositories/github_api.py
@@ -1,14 +1,20 @@
+import datetime
 import requests
 
 
+# TODO get current username
 def get_repository(repository_name):
+
+    payload = {'since': datetime.datetime.now()-datetime.timedelta(days=30)}
+
     response = requests.get(
-        f'https://api.github.com/repos/tuliolages/{repository_name}'
+        f'https://api.github.com/repos/tuliolages/{repository_name}',
+        params=payload
     )
 
     if response.status_code != 200:
         raise Exception("Repository does not exist")
-    
+
     return response.json()
 
 
@@ -16,8 +22,8 @@ def get_commits(repository_name):
     response = requests.get(
         f'https://api.github.com/repos/tuliolages/{repository_name}/commits'
     )
-    
+
     if response.status_code != 200:
         raise Exception("Unable to retrieve commits")
-    
+
     return response.json()
