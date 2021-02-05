70acdb89aede36a17baa234017a423447454fb80
Tulio Lages <tuliolages@joystreet.com.br>
2021-02-05 00:06:00 -0300

    move commits payload to correct method

--- repositories/github_api.py
+++ repositories/github_api.py
@@ -5,11 +5,8 @@ import requests
 # TODO get current username
 def get_repository(repository_name):
 
-    payload = {'since': datetime.datetime.now()-datetime.timedelta(days=30)}
-
     response = requests.get(
-        f'https://api.github.com/repos/tuliolages/{repository_name}',
-        params=payload
+        f'https://api.github.com/repos/tuliolages/{repository_name}'
     )
 
     if response.status_code != 200:
@@ -19,11 +16,14 @@ def get_repository(repository_name):
 
 
 def get_commits(repository_name):
+
+    payload = {'since': datetime.datetime.now()-datetime.timedelta(days=30)}
     response = requests.get(
-        f'https://api.github.com/repos/tuliolages/{repository_name}/commits'
+        f'https://api.github.com/repos/tuliolages/{repository_name}/commits',
+        params=payload
     )
 
     if response.status_code != 200:
         raise Exception("Unable to retrieve commits")
-
+    print(response.json())
     return response.json()
