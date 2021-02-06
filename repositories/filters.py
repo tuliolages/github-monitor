c5ce1e0b9bc234fee4ae403865576d803f3c343e
Tulio Lages <tuliolages@joystreet.com.br>
2021-02-06 17:03:48 -0300

    remove useless filters file

--- repositories/filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters import rest_framework as filters
-
-from .models import Commit
-
-
-class CommitFilter(filters.FilterSet):
-    class Meta:
-        model = Commit
-        fields = ('category', 'in_stock')
